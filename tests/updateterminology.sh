8afc6084023df1fc04d737a22d50c8918d586445
oliveregger <oliver.egger@ahdis.ch>
2024-03-26 14:12:06 +0100

    ValueSet 2403014

--- tests/updateterminology.sh
+++ tests/updateterminology.sh
@@ -66,6 +66,42 @@ curl --request GET \
   --key $KEY \
   -o ../input/resources/ValueSet-$VALUESET.json
 
+VALUESET="ch-elm-interpretation-codes-vs"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ValueSet/$VALUESET \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/ValueSet-$VALUESET.json
+
+VALUESET="ch-elm-results-camp-diar-org"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ValueSet/$VALUESET \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/ValueSet-$VALUESET.json
+
+VALUESET="ch-elm-results-camp-org"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ValueSet/$VALUESET \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/ValueSet-$VALUESET.json
+
+VALUESET="ch-elm-results-complete-spec"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ValueSet/$VALUESET \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/ValueSet-$VALUESET.json
+
 CONCEPTMAP="ch-elm-expecting-organism-specification-to-results-completion-vs"
 curl --request GET \
   --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ConceptMap/$CONCEPTMAP \
@@ -93,6 +129,15 @@ curl --request GET \
   --key $KEY \
   -o ../input/resources/ConceptMap-$CONCEPTMAP.json
 
+CONCEPTMAP="ch-elm-results-to-interpretation-code"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/ConceptMap/$CONCEPTMAP \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/ConceptMap-$CONCEPTMAP.json
+
 CODESYSTEM="ch-elm-foph-business-rules"
 curl --request GET \
   --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/CodeSystem/$CODESYSTEM \
@@ -112,6 +157,15 @@ curl --request GET \
   -o ../input/resources/CodeSystem-$CODESYSTEM.json
 
 CODESYSTEM="ch-elm-results-completion-vs"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/CodeSystem/$CODESYSTEM \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/CodeSystem-$CODESYSTEM.json
+
+CODESYSTEM="ch-elm-interpretation-codes-vs"
 curl --request GET \
   --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/CodeSystem/$CODESYSTEM \
   --header 'accept: application/fhir+json' \
